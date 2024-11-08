9507110c8a66167747bdba56a73598c6129f1b96
Artur Matysiak <inz.matys@gmail.com>
2024-11-08 14:55:42 +0100

    add style to components Movie v.2

--- src/components/Movie/Movie.tsx
+++ src/components/Movie/Movie.tsx
@@ -3,15 +3,20 @@ import style from "./Movie.module.css";
 export const Movie = () => {
   return (
     <div className={style.movie}>
-      <div className="movie_description">
-        <p>Tytuł</p>
-        <p>rok</p>
-        <p>gatunek</p>
+      <div className={style.movie_description}>
+        <div className={style.movie_data}>
+          <p>Sweet Tooth</p>
+          <p>2021</p>
+          <p>serial</p>
+        </div>
 
-        <div className="rating">
-          <p>gwiazdki</p>
-          <span>glosy (2/4)</span>
-          <p className="comments">komentarze</p>
+        <div className={style.rating}>
+          <img src="/star-empty.svg" alt="star" className={style.star} />
+          <img src="/star-empty.svg" alt="star" className={style.star} />
+          <img src="/star-empty.svg" alt="star" className={style.star} />
+          <span>(2/4)</span>
+          {/* <p className={style.comments}>komentarze</p> */}
+          <img src="/comments.svg" alt="comments" className={style.comments} />
         </div>
       </div>
     </div>
