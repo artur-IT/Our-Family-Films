3446aebb274199a46b614bb8ac13b29bbcbc0f18
Artur Matysiak <inz.matys@gmail.com>
2024-11-08 13:24:41 +0100

    add style to components Movie

--- src/components/Movie/Movie.tsx
+++ src/components/Movie/Movie.tsx
@@ -3,7 +3,17 @@ import style from "./Movie.module.css";
 export const Movie = () => {
   return (
     <div className={style.movie}>
-      <h2>Movie Component</h2>
+      <div className="movie_description">
+        <p>Tytuł</p>
+        <p>rok</p>
+        <p>gatunek</p>
+
+        <div className="rating">
+          <p>gwiazdki</p>
+          <span>glosy (2/4)</span>
+          <p className="comments">komentarze</p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Shelf/Shelf.tsx
+++ src/components/Shelf/Shelf.tsx
@@ -1,16 +1,40 @@
 import { Movie } from "../Movie/Movie";
 import style from "./Shelf.module.css";
+import { useRef } from "react";
 
 export const Shelf = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const scroll = (direction: "left" | "right") => {
+    if (containerRef.current) {
+      const scrollAmount = 300; // możesz dostosować wartość
+      containerRef.current.scrollBy({
+        left: direction === "left" ? -scrollAmount : scrollAmount,
+        behavior: "smooth",
+      });
+    }
+  };
+
   return (
     <>
       <div className={style.shelf}>
-        {/* <h2>Shelf</h2> */}
-        <Movie />
-        <Movie />
-        <Movie />
+        <button className={`${style.scroll_button} ${style.scroll_left}`} onClick={() => scroll("left")}>
+          ←
+        </button>
+
+        <div className={style.shelf_movie_container} ref={containerRef}>
+          <Movie />
+          <Movie />
+          <Movie />
+          <Movie />
+        </div>
+
+        <button className={`${style.scroll_button} ${style.scroll_right}`} onClick={() => scroll("right")}>
+          →
+        </button>
       </div>
       <div className={style.shelf_bottom}></div>
+      <div className={style.year}>2024</div>
     </>
   );
 };
