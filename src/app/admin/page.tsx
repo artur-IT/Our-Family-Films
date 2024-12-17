132488b912817a8015906080b728188636984614
Artur Matysiak <inz.matys@gmail.com>
2024-12-17 15:30:07 +0100

    move EditMovie buttons from Header to /admin/page.tsx

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,12 +2,22 @@
 import { useEffect } from "react";
 import { useEditMode } from "@/context/EditMovieContext";
 
-export default function AdminPanel() {
+interface PropsTypes {
+  setShowAddMovie: () => void;
+  isEditMode: boolean;
+}
+
+export default function AdminPanel({ setShowAddMovie, isEditMode }: PropsTypes) {
   const { toggleEditMode } = useEditMode();
 
   useEffect(() => {
     toggleEditMode();
     // Tutaj później dodamy sprawdzanie autoryzacji
   }, []);
-  return <></>;
+  return (
+    <>
+      <button onClick={setShowAddMovie}>Dodaj film</button>
+      <button onClick={toggleEditMode}>{isEditMode ? "Zakończ edycję" : "Edytuj filmy"}</button>
+    </>
+  );
 }
--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -5,24 +5,16 @@ import style from "./Header.module.css";
 import Link from "next/link";
 import { useState } from "react";
 import { useLoginState } from "@/context/LoginStateContext";
-import { set } from "react-hook-form";
+import AdminPanel from "@/app/admin/page";
 
-interface PropsTypes {
-  setShowAddMovie: () => void;
-}
-
-export const Header = ({ setShowAddMovie }: PropsTypes) => {
-  const { isEditMode, toggleEditMode } = useEditMode();
+export const Header = ({ setShowAddMovie }: { setShowAddMovie: () => void }) => {
+  const { isEditMode } = useEditMode();
   const [showPanelLogin, setShowPanelLogin] = useState<boolean>(false);
   const { isLoggedIn, setIsLoggedIn } = useLoginState();
   const pathname = usePathname();
 
-  //
-
   const handleLinkLogin = () => {
-    if (isLoggedIn) {
-      setIsLoggedIn(!isLoggedIn);
-    }
+    if (isLoggedIn) setIsLoggedIn(!isLoggedIn);
     setShowPanelLogin(!showPanelLogin);
   };
 
@@ -32,12 +24,7 @@ export const Header = ({ setShowAddMovie }: PropsTypes) => {
         <nav>
           <p>Our Family Films</p>
 
-          {pathname === "/admin" && isLoggedIn && (
-            <>
-              <button onClick={setShowAddMovie}>Dodaj film</button>
-              <button onClick={toggleEditMode}>{isEditMode ? "Zakończ edycję" : "Edytuj filmy"}</button>
-            </>
-          )}
+          {pathname === "/admin" && isLoggedIn && <AdminPanel setShowAddMovie={setShowAddMovie} isEditMode={isEditMode} />}
 
           <Link href={showPanelLogin ? "/" : "/auth"} onClick={handleLinkLogin}>
             {isLoggedIn ? "Wyloguj" : "Zaloguj"}
